3b786bc6b77b1469b88fe6f791f205031ab41a70
Theo <trgrf1@gmail.com>
2021-09-28 15:18:41 -0400

    saving type name object in model

--- models/workout.js
+++ models/workout.js
@@ -20,7 +20,9 @@ const workoutSchema = new Schema(
         },
         name: {
           // TODO: For the schema of this attribute, define type and trim and required
-          
+          type: String,
+          trim: true,
+          required: "Workout name is Required"
         },
         duration: {
           // TODO: For the schema of this attribute, define type as number and its required
